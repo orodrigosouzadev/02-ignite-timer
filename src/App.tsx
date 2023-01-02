33d1cd47209e852fba05cba9d27fb722e08677e7
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2023-01-02 17:05:49 -0300

    feat: implement countdown and history

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
