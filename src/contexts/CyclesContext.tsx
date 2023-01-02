be713f3dcebf5dd6a369bdd4f7b8123743daa18f
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2023-01-02 17:30:15 -0300

    refactor: improve code quality

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -38,12 +38,13 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
+  const initialState = {
+    cycles: [],
+    activeCycleId: null,
+  }
   const [cyclesState, dispatch] = useReducer(
     cyclesReducer,
-    {
-      cycles: [],
-      activeCycleId: null,
-    },
+    initialState,
     () => {
       const storedStateAsJSON = localStorage.getItem(
         '@ignite-timer:cycles-state-1.0.0',
@@ -52,10 +53,7 @@ export function CyclesContextProvider({
       if (storedStateAsJSON) {
         return JSON.parse(storedStateAsJSON)
       }
-      return {
-        cycles: [],
-        activeCycleId: null,
-      }
+      return initialState
     },
   )
 
